aada17bcb0f4e3e4f92cb990bf92c2dac6521836
Justin Djeumene <hugues-justin.djeumene-tchebou.1@ulaval.ca>
2019-09-17 15:47:28 -0600

    Integration du framework Buefy.js et router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "/components/Home";
-import Album from "/components/Album";
-import Artist from "/components/Artist";
+import Home from "@/components/Home";
+import Album from "@/components/Album";
+import Artist from "@/components/Artist";
 
 Vue.use(Router);
 

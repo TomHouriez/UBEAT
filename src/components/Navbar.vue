c5e6cc54095a29a0d6db01138145c67804c02c69
Justin Djeumene <hugues-justin.djeumene-tchebou.1@ulaval.ca>
2019-09-19 23:05:44 -0600

    Navbar -- version finale bien testee!

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,271 +1,263 @@
-<!--<template>
-  <section class="team-navbar">
-    <b-icon icon="settings" class="myIcon"></b-icon>
-    <div class="boxe box1">
-      <img class="logo"
-           src="../assets/A19Team01-logo.png"
-           alt="Lightweight UI components for Vue.js based on Bulma"
-      >
-    </div>
-    <div class="boxe box2">
-      <a class="read">Liste de lectures</a>
-    </div>
-    <section id="content" class="boxe box3">
-      <form>
-        <input
-          type="text"
-          required
-          id="search-box"
-          class="search-box"
-          placeholder="Selectionner or rechercher les mots ici...."
-          list="dropdown-list"
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img
+          src="../assets/A19Team01-logo-white.png"
+          alt="A19-Team-01"
+          class="logo"
         >
-        <datalist id="dropdown-list">
-          <option value="Universite Laval"></option>
-          <option value="Le Basketball dans les rues"></option>
-          <option value="L'intelligence des affaires"></option>
-          <option value="L'Universite Laval comparer avec Toronto"></option>
-          <option value="Les nouvelles de l'Universite Laval"></option>
-          <option value="Le campus universitaire"></option>
-          <option value="Les repas moins dispendieux pour l'Universite"></option>
-        </datalist>
-        <button
-          type="reset"
-          class="position-button"
-        >
-          X
-        </button>
-        <p id="no-result"> Pas de resultats</p>
-      </form>
-    </section>
-    <section class="boxe box4">
-      <div class="navbar-menu is-active">
-        <div class="navbar-end">
-          <b-dropdown
-            v-model="navigation"
-            position="is-bottom-left"
-            aria-role="menu">
-            <a
-              class="navbar-item"
-              slot="trigger"
-              role="button">
-              <span>
-                 <img src="https://s3.amazonaws.com/uifaces/faces/twitter/heyimjuani/128.jpg" width="28" height="28">
-              </span>
-              <b-icon icon="menu-down" type="is-white"></b-icon>
-            </a>
+      </a>
+      <a class="navbar-item listes-tablet-show-only is-white">
+        Listes de lectures
+      </a>
+      <a class="navbar-item search-tablet-show-only">
+        <div class="control has-icons-left"  >
+          <input class="input" type="search" placeholder="Select or search for musics.....">
+          <span class="icon is-left">
+              <i class="fas fa-search"></i>
+             </span>
 
-            <b-dropdown-item custom aria-role="menuitem">
-              Logged as <b>{{nom}}</b>
-            </b-dropdown-item>
-            <hr class="dropdown-divider" aria-role="menuitem">
-            <b-dropdown-item value="settings">
-              <b-icon icon="settings"></b-icon>
-              Profile
-            </b-dropdown-item>
-            <b-dropdown-item value="logout" aria-role="menuitem">
-              <b-icon icon="logout"></b-icon>
-              Deconnexion
-            </b-dropdown-item>
-          </b-dropdown>
+        </div>
+      </a>
+
+      <div class="dropdown is-hoverable user-tablet-show-only">
+        <div class="dropdown-trigger">
+          <a  aria-haspopup="true" aria-controls="dropdown-menu">
+                          <span>
+                             <img src="https://s3.amazonaws.com/uifaces/faces/twitter/heyimjuani/128.jpg"  class="img-menu">
+                          </span>
+            <b-icon icon="menu-down" type="is-white"></b-icon>
+            <span class="icon is-small">
+                            <i class="fas fa-angle-down" aria-hidden="true"></i>
+                          </span>
+          </a>
+        </div>
+        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+          <div class="dropdown-content">
+            <a href="#" class="dropdown-item">
+              Active dropdown item
+            </a>
+            <a href="#" class="dropdown-item">
+              Settings
+            </a>
+            <hr class="dropdown-divider">
+            <a href="#" class="dropdown-item">
+              Sign out
+            </a>
+          </div>
         </div>
       </div>
-    </section>
-  </section>
-</template> -->
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navMenu" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item desktop is-white" href="#">
+          Listes de lectures
+        </a>
+        <a class="navbar-item desktop tablet">
+          <div class="control has-icons-left"  >
+            <input class="input search" type="search" placeholder="Select or search for musics....">
+            <span class="icon is-left">
+                <i class="fas fa-search"></i>
+             </span>
+
+          </div>
+        </a>
+
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            <span class="icon is-medium"><i class="fa fa-user"></i></span>
+            <span>
+                             <img src="https://s3.amazonaws.com/uifaces/faces/twitter/heyimjuani/128.jpg"  class="img-menu">
+                        </span>
+          </a>
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              <span class="icon"><i class="fa fa-sign-out"></i></span>
+              <span>Sign out</span>
+            </a>
+            <a class="navbar-item">
+              <span class="icon"><i class="fa fa-cog"></i></span>
+              <span>Settings</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
 
 <script>
-export default {
-name: 'NavBar',
-props: {
-    msg: String,
-    type: String,
-},
-data(){
-  return {
-      nom: "Justin Djeumene",
-      navigation: 'home'
-  }
-},
-mounted(){
+    export default {
+        name: "Navbar2",
+        mounted() {
+            document.addEventListener('DOMContentLoaded', () => {
 
-},
-methods: {
+                // Get all "navbar-burger" elements
+                const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-}
-}
+                // Check if there are any navbar burgers
+                if ($navbarBurgers.length > 0) {
+
+                    // Add a click event on each of them
+                    $navbarBurgers.forEach( el => {
+                        el.addEventListener('click', () => {
+
+                            // Get the target from the "data-target" attribute
+                            const target = el.dataset.target;
+                            const $target = document.getElementById(target);
+
+                            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                            el.classList.toggle('is-active');
+                            $target.classList.toggle('is-active');
+
+                        });
+                    });
+                }
+
+            });
+        }
+    }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-.search-box {
-  width: 550px;
-  min-height: 35px;
-  border-radius: 4px;
-}
-
-input:focus {
-  outline-offset: 0;
-  border: #270B99 2px solid;
-}
-
-:focus {
-  outline: none;
-}
-
-input {
-  border: #ddd 2px solid;
-  padding: 4px 5px;
-  font-size: 15px;
-}
-
-p {
-  color: #fff;
-  display: none;
-  margin: 2px;
-}
-
-button {
-  width: 20px;
-  color: #fff;
-  background-color:crimson;
-  border-style: none;
-  padding-left: auto;
-}
-
-.position-button {
-  position: relative;
-  z-index: 1;
-  left: -35px;
-  display: none;
-}
-
-option {
-  font-size: 20px;
-}
-
-.logo {
-  height: 19px;
-}
-
-.team-navbar {
-  display: flex;
-  flex-direction: row;
-  background-image: linear-gradient(90deg, #5C101A 0, #270B99 100%);
-}
-
-.boxe {
-  flex: 1 0 0;
-  padding: 10px;
-}
-.box3 {
-  flex: 4;
-}
-.box1 {
-  flex: 0.5;
-}
-.box2 {
-  flex: 0.5;
-}
-.box4{
-  padding: 0;
-}
-p {
-  color: white;
-}
-
-.myIcon {
-  display: none;
-}
-.read {
-  color: white;
-}
-div:hover {
-
-}
-
-.navbar-end {
-  background-color: transparent;
-}
-@media only screen and (max-width: 600px) {
-  .team-navbar{
-    flex-wrap:wrap;
-    justify-content: center;
+  .logo {
+    height: 19px;
   }
-  .boxe {
-    flex:1 1 50%;
-  }
-  .box4 {
-    max-width: 50px;
-    height: 50px;
-    color: transparent;
-  }
-  .search-box{
-    width: 250px;
-    min-height: 35px;
-    border-radius: 4px;
-  }
-}
-
-/* Medium devices (landscape tablets, 768px and up) */
-@media only screen and (min-width: 601px) {
-  .team-navbar{
-    flex-wrap:wrap;
-    height: 75px;
+  .navbar {
+    background-color: #333;
+    padding-left: 15px;
+    padding-right: 15px;
   }
 
-  .boxe {
-    flex:1 1 25%;
-  }
-  .box4 {
 
-  }
-  .box1,
-  .box2 {
-    flex: 0.75;
-  }
-  .box3 {
-    flex: 2;
-  }
-  .search-box {
-    max-width: 250px;
-    min-height: 35px;
-    border-radius: 4px;
-  }
-}
+  /* Mobile devices up to 768px */
+  @media only screen and (max-width: 767px) {
+    .navbar-item.search-tablet-show-only, .user-tablet-show-only, .navbar-item.listes-tablet-show-only {
+      display: none;
+    }
+    .img-menu {
+      width: 28px;
+      border-radius: 50%;
+      margin-top: 15px;
+      margin-left: 50px;
+    }
 
-  /* Large devices (laptops/desktops, 992px and up) */
-@media only screen and (min-width: 992px) {
-  .team-navbar {
-    display: flex;
-    flex-direction: row;
-    background-image: linear-gradient(90deg, #5C101A 0, #270B99 100%);
-    width: 100vw;
-    padding-right: 30px;
-    margin-left: 0;
+    a.navbar-burger.burger {
+      color: white;
+    }
 
   }
 
-  .boxe {
-    flex: 1 0 0;
-    padding: 10px;
+  /* Tablets devices from 769px */
+  @media only screen and (min-width: 768px) {
+
+    .user-tablet-show-only {
+      margin-left: 90px;
+    }
+    a.navbar-burger.burger {
+      display: none;
+    }
+    .img-menu {
+      width: 28px;
+      border-radius: 50%;
+      margin-top: 12px;
+
+    }
+
+    .search {
+      width: 300px;
+      margin-left: 50px;
+    }
+    .search:hover {
+      background-color: transparent;
+    }
+    .navbar-item:hover {
+      background-color: transparent;
+      color: white;
+    }
+    .navbar-item:focus, .navbar-item:active, .navbar-item:focus-within {
+      background-color: transparent;
+    }
+    .navbar-item {
+      color: white;
+    }
+
   }
 
-  .box3 {
-    flex: 4;
+
+  /* Desktop devices from 1024px */
+  @media only screen and (min-width: 1024px) {
+    a.navbar-burger.burger {
+      display: none;
+    }
+    .search-tablet-show-only, .user-tablet-show-only, .listes-tablet-show-only {
+      display: none;
+    }
+
+    input.search {
+      width: 500px;
+      margin-left: 100px;
+    }
+
+    .search:hover {
+      background-color: transparent;
+    }
+    .navbar-item:hover {
+      background-color: transparent;
+      color: white;
+    }
+    .navbar-item:focus, .navbar-item:active, .navbar-item:focus-within {
+      background-color: transparent;
+    }
+    .navbar-item {
+      color: white;
+    }
   }
 
-  .box1 {
-    flex: 0.5;
+  /* Wide screens devices from 1216px */
+  @media only screen and (min-width: 1216px) {
+    input.search {
+      width: 600px;
+      margin-left: 150px;
+    }
+    .search:hover {
+      background-color: transparent;
+    }
+    .navbar-item:hover {
+      background-color: transparent;
+      color: white;
+    }
+    .navbar-item:focus, .navbar-item:active, .navbar-item:focus-within {
+      background-color: transparent;
+    }
+    .navbar-item {
+      color: white;
+    }
   }
 
-  .box2 {
-    flex: 0.5;
+  /* Full HD devices from 1408px */
+  @media only screen and (min-width: 1408px) {
+    .search:hover {
+      background-color: transparent;
+    }
+    .navbar-end > .navbar-item:hover {
+      background-color: transparent;
+    }
+    .navbar-end > .navbar-item:focus, .navbar-item:active, .navbar-item:focus-within {
+      background-color: transparent;
+    }
+    .navbar-item.has-dropdown:hover, .navbar-link {
+      background-color: transparent;
+    }
   }
-
-  .box4 {
-    padding: 0;
-  }
-}
 </style>

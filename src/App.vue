6d790ce471a6e52721abae9ed95cafc76c18deae
Pascal Cauden <pascal.cauden.1@ulaval.ca>
2019-09-25 21:53:43 -0400

    revert app.vue

--- src/App.vue
+++ src/App.vue
@@ -15,16 +15,4 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700');
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 0;
-  height: 100vh;
-  overflow: hidden;
-}
-</style>
+<style></style>

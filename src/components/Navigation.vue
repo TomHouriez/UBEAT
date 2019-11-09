d8500d6292500d80cd270fdf27b8564eb542011e
momo86 <mohamed.djama-ibrahim.1@ulaval.ca>
2019-11-09 16:58:50 -0500

    navbar style

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar type="is-dark">
+  <b-navbar :fixed-top="true" type="is-dark">
     <template slot="start">
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
         <b-icon pack="fas" icon="home" type="info" />
@@ -7,39 +7,91 @@
       <b-navbar-item tag="router-link" :to="{ path: '/playlists' }">
         Playlists
       </b-navbar-item>
-      <b-navbar-item>
-        <b-field>
-          <b-input
-            placeholder="Search..."
-            type="search"
-            icon-pack="fas"
-            icon="search"
+
+      <div class="searchLine">
+        <b-autocomplete
+          v-model="searchInput"
+          icon-pack="fas"
+          icon="search"
+          class="autocomplete"
+          placeholder="Search"
+          :data="searchData"
+          @typing="getSearchData"
+          @select="select"
+        >
+        </b-autocomplete>
+        <b-button class="searchButton" v-on:click="search()">Search</b-button>
+      </div>
+
+      <b-navbar-dropdown hoverable label="Filters" style="width:120px">
+        <b-navbar-item>
+          <b-checkbox v-model="filters" native-value="artists"
+            >Artists</b-checkbox
           >
-          </b-input>
-        </b-field>
-      </b-navbar-item>
+        </b-navbar-item>
+        <b-navbar-item>
+          <b-checkbox v-model="filters" native-value="albums"
+            >Albums</b-checkbox
+          >
+        </b-navbar-item>
+        <b-navbar-item>
+          <b-checkbox v-model="filters" native-value="tracks"
+            >Tracks</b-checkbox
+          >
+        </b-navbar-item>
+        <b-navbar-item>
+          <b-checkbox v-model="filters" native-value="users">Users</b-checkbox>
+        </b-navbar-item>
+      </b-navbar-dropdown>
     </template>
 
     <template slot="end">
-      <b-navbar-dropdown label="Joe Blo">
+      <b-navbar-dropdown hoverable label="Joe Blo" style="width:150px">
         <b-navbar-item href="#">
-          Settings
+          <b-icon pack="fas" class="fa fa-cog" type="info" />
+          <span>Settings</span>
         </b-navbar-item>
         <b-navbar-item href="#">
-          Profile
+          <b-icon pack="fas" class="fa fa-user" type="info" />
+          <span>Profile</span>
         </b-navbar-item>
         <b-navbar-item href="#">
-          Sign out
+          <b-icon pack="fas" class="fa fa-times" type="info" />
+          <span>Sign Out</span>
         </b-navbar-item>
       </b-navbar-dropdown>
     </template>
   </b-navbar>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      filters: ["artists", "albums", "tracks", "users"],
+      searchInput: ""
+    };
+  }
+};
+</script>
 
 <style scoped>
-/* .navbar {
-  background-color: gray;
-} */
+.searchLine {
+  width: 100%;
+  min-width: 400px;
+  height: 50px;
+}
+
+.autocomplete {
+  margin-top: 10px;
+  margin-left: 30px;
+  float: left;
+  width: 60%;
+  min-width: 400px;
+}
+.searchButton {
+  width: 70px;
+  float: left;
+  margin-top: 10px;
+}
 </style>

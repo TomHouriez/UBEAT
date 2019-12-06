d0a1ae9bb818375ffab0b9200d97970d4f02c6f2
Pascal Cauden <pascal.cauden.1@ulaval.ca>
2019-12-06 16:58:31 -0500

    Add strArtistThumb function and misc cleanup

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,56 +1,48 @@
 <template>
   <div class="userContainer">
-    <div class="iconContainer">      
+    <div class="iconContainer">
       <i class="fas fa-3x fa-user"></i>
       <p class="name">{{ userData.name }}</p>
       <p class="email">{{ userData.email }}</p>
-      <b-button
-      v-on:click="followThisUser(userData.id)"
-      v-if="!isFollowed"
-      >
-      Follow
+      <b-button v-on:click="followThisUser(userData.id)" v-if="!isFollowed">
+        Follow
       </b-button>
-      <b-button
-      v-on:click="unfollowThisUser(userData.id)"
-      v-if="isFollowed"
-      >
-      Unfollow
+      <b-button v-on:click="unfollowThisUser(userData.id)" v-if="isFollowed">
+        Unfollow
       </b-button>
     </div>
   </div>
 </template>
 
 <script>
-import {followUser, unfollowUser, isUserFollowed} from "../scripts/FollowApi";
+import { followUser, unfollowUser, isUserFollowed } from "../scripts/FollowApi";
 
 export default {
   props: ["userData"],
   data() {
     return {
-      isFollowed: false,
+      isFollowed: false
     };
   },
   methods: {
     async followThisUser(userID) {
       let isFollowed = await isUserFollowed(userID);
-      if(!isFollowed) {
+      if (!isFollowed) {
         const jsonResponse = await followUser(userID);
         this.isFollowed = true;
         this.$buefy.snackbar.open(`User followed`);
-      }
-      else {
+      } else {
         this.$buefy.snackbar.open(`Error`);
       }
     },
     async unfollowThisUser(userID) {
       let isFollowed = await isUserFollowed(userID);
-      if(!isFollowed) {
+      if (!isFollowed) {
         this.$buefy.snackbar.open(`Error`);
         // const jsonResponse = await followUser(userID);
         // this.isFollowed = true;
         // this.$buefy.snackbar.open(`User followed`);
-      }
-      else {
+      } else {
         const jsonResponse = await unfollowUser(userID);
         this.isFollowed = false;
         this.$buefy.snackbar.open(`User unfollowed`);
@@ -58,29 +50,28 @@ export default {
     }
   },
   async created() {
-    this.isFollowed = await isUserFollowed(this.$props.userData.id)
+    this.isFollowed = await isUserFollowed(this.$props.userData.id);
   }
 };
 </script>
 
 <style scoped>
-  .userContainer {
-    display: block;
-    margin:20px;
-    padding: 10px;
-    border-style: solid;
-    border-width: 1px;
-    text-align: center;
-  }
+.userContainer {
+  display: block;
+  margin: 20px;
+  padding: 10px;
+  border-style: solid;
+  border-width: 1px;
+  text-align: center;
+}
 
-  .name {
-    font-weight: bold;
-    color: black;
-    padding: 2px;
-  }
+.name {
+  font-weight: bold;
+  color: black;
+  padding: 2px;
+}
 
-  .email {
-    padding-bottom: 5px;
-  }
-
+.email {
+  padding-bottom: 5px;
+}
 </style>
--- src/scripts/ArtistsApi.js
+++ src/scripts/ArtistsApi.js
@@ -38,16 +38,79 @@ export const fetchMbzArtist = async mbid => {
   return jsonResponse;
 };
 
-// fetch artist albums from Musicbrainz
+export const getArtistInfos = async id => {
+  let artistData = await fetchArtistData(id);
+  if (artistData.resultCount !== 0) {
+    artistData = artistData.results[0];
+    let albums = await fetchArtistAlbums(id);
+    if (albums.resultCount !== 0) {
+      albums = albums.results;
+      // Sort albums in the inverse of released year order (most recent years first)
+      albums.sort(
+        (a, b) => b.releaseDate.substring(0, 4) - a.releaseDate.substring(0, 4)
+      );
+    }
+  }
+  // Try to fetch Musicbrainz ID from TADB
+  let tadbData = await fetchArtistMBID(
+    encodeURIComponent(artistData.artistName)
+  );
 
-// Infer Musicbrainz artist from Name and Albums
-// export const inferMbzArtist = async artistName => {
-//   const response = await fetch(
-//     `${MB_BASE_URL}/artist/?query=${artistName}&fmt=json`
-//   );
-//   const jsonResponse = await response.json();
+  if (tadbData.artists != null) {
+    // we assume the first artist is the right one
+    tadbData = tadbData.artists[0];
 
-//   for (let artist of jsonResponse.artists) {
-//     let albums =
-//   }
-// };
+    // now that we have a mbid we fetch the infos from MusicBrainz
+    let mbData = await fetchMbzArtist(tadbData.strMusicBrainzID);
+
+    let artistInfos = new Object();
+    artistInfos.artistName = artistData.artistName;
+    artistInfos.strArtistThumb = tadbData.strArtistThumb;
+    artistInfos.artistItuneLinkUrl = artistData.artistLinkUrl;
+    // artistLife: Object with all artist's life detail (easier to process in the template)
+    artistInfos.artistLife = new Object();
+    artistInfos.artistLife.begin = mbData["life-span"].begin;
+    artistInfos.artistLife.ended = mbData["life-span"].ended;
+
+    // Displayed text is different for a personn or a group
+    if (mbData.type == "Group") {
+      artistInfos.artistLife.typeText = "Formed";
+      artistInfos.artistLife.status = "Disbanded (";
+      // We also want to display the group members
+      artistInfos.members = [];
+      for (let i = 0; i < mbData.relations.length; i++) {
+        if (mbData.relations[i].type == "member of band") {
+          var member = mbData.relations[i].artist.name;
+          // Members may have had several tenure/positions, we only want to list each member once
+          if (
+            artistInfos.members.indexOf(member) == -1 &&
+            artistInfos.members.indexOf(member + " (Inactive)") == -1
+          ) {
+            if (mbData.relations[i].ended) {
+              member += " (Inactive)";
+            }
+            artistInfos.members.push(member);
+          }
+        }
+      }
+    } else {
+      artistInfos.artistLife.typeText = "Born";
+      artistInfos.artistLife.status = "Dead (";
+    }
+
+    if (mbData["life-span"].ended) {
+      artistInfos.artistLife.status =
+        artistInfos.artistLife.status + mbData["life-span"].end + ")";
+    } else {
+      artistInfos.artistLife.status = "Active";
+    }
+    // We only keep the first 8 genres
+    artistInfos.genres = mbData.genres;
+    if (artistInfos.genres.length > 8) {
+      artistInfos.genres.splice(8);
+    }
+    return artistInfos;
+  } else {
+    return null;
+  }
+};
